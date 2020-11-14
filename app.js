abdb8c68936e005e27dd08e0541209107ec184ab
Valeria <valery0007@gmail.com>
2020-11-14 23:10:05 +0100

    posts

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+const posts = [{
+    "id": "1581461442206",
+    "title": "This is a new blog post",
+    "content": "This is the content",
+    "post_image": "uploads/post-image-1581461442199.jpg",
+    "added_date": "1581461442206"
+}]
+
+
+app.get("/api/posts", (req, res) => {
+    res.status(200).send(posts);
+});
+
+
+app.listen(3000, () =>
+    console.log("Listening on http://localhost:3000"))
