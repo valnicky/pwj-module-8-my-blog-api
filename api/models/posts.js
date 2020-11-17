aa9dad5aa7937568dc9384731adfad71997b7c01
Valeria <valery0007@gmail.com>
2020-11-17 12:48:08 +0100

    module.exports

---
+++ api/models/posts.js
@@ -0,0 +1,14 @@
+const PATH = "./data.json";
+
+class Post {
+    get() {
+        /*get posts */
+
+    }
+
+    getIndividualBlog() {
+        /*get one blog post */
+    }
+}
+
+module.exports = Post;
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const Post = require('./api/models/posts');
+const postsData = new Post();
 
 const posts = [{
     "id": "1581461442206",
@@ -10,11 +11,9 @@ const posts = [{
     "added_date": "1581461442206"
 }]
 
-
 app.get("/api/posts", (req, res) => {
     res.status(200).send(posts);
 });
-
 
 app.listen(3000, () =>
     console.log("Listening on http://localhost:3000"));
