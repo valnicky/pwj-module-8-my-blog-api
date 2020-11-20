27f1cbd7b936536b0be00437bc007fa9c8c98e64
Valeria <valery0007@gmail.com>
2020-11-20 10:21:32 +0100

    read write json

--- api/models/posts.js
+++ api/models/posts.js
@@ -1,13 +1,32 @@
-const PATH = "./data.json";
+const PATH = "./exercise/data.json";
+const fs = require('fs');
 
 class Post {
     get() {
         /*get posts */
-
+    return this.readData();
     }
 
     getIndividualBlog() {
         /*get one blog post */
+    }
+    
+    add(newPost){
+        /*add new post*/
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
+    }
+    
+    readData(){
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+            return posts;
+    }
+    
+    storeData(rawData){
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 }
 
--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const app = express();
 const Post = require('./api/models/posts');
 const postsData = new Post();
 
-const posts = [{
-    "id": "1581461442206",
-    "title": "This is a new blog post",
-    "content": "This is the content",
-    "post_image": "uploads/post-image-1581461442199.jpg",
-    "added_date": "1581461442206"
-}]
 
-app.get("/api/posts", (req, res) => {
-    res.status(200).send(posts);
+
+app.get("/api/posts", (req, res) => { 
+    res.status(200).send(postsData.get());
 });
 
 app.listen(3000, () =>
