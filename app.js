234cfc63075c16ba9ceb52f2bb1aab1bd2109329
Valeria <valery0007@gmail.com>
2020-11-25 19:04:23 +0100

    app.js

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
-const express = require('express');
-const app = express();
-const Post = require('./api/models/posts');
-const postsData = new Post();
+const express = require('express')
+const app = express()
+const Post = require('./api/models/posts')
+const postsData = new Post()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
 });
 
 app.use("/uploads", express.static('uploads'));
 
 app.get("/api/posts", (req, res) => {
-    res
-        .status(200)
-        .send(postsData.get());
+    res.status(200).send(postsData.get());
 });
 
 app.get("/api/posts/:post_id", (req, res) => {
     const postId = req.params.post_id;
     const foundPost = postsData.getIndividualBlog(postId);
     if (foundPost) {
-        res
-            .status(200)
-            .send(foundPost);
+        res.status(200).send(foundPost);
     } else {
-        res
-            .status(404)
-            .send("Not Found");
+        res.status(404).send("Not Found");
     }
 });
 
